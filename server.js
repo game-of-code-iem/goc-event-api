860d52f315ba6af968e7a0d531a18d17424e9798
3jedgcm <clement.merlet26@gmail.com>
2019-05-03 09:20:31 +0200

    [LOCAL]

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
-const CONSTANT = require("./constant/constant")
 const express = require('express');
-const controller = require('./controller/controller');
-
 const app = express();
-
-// Pour autoriser les appels http
-//const cors = require('cors')
-//app.use(cors({credentials: true, origin: true}))
-
 var http = require("http").createServer(app);
 const io = require('socket.io')(http, { origins: '*:*'});
 
-
-// CONTROLLER = controller
- io.on("connection",(socket) => {
-    console.log("on connection")
-     // Retourne le status du server par defaut 200
-     socket.emit("status", "200")
+io.on("connection",(socket) => {
+   console.log("on connection")
+   socket.emit("status", "200")
 
 })
 
-
-
-http.listen("8080","localhost");
+http.listen("4444","192.168.43.211");
